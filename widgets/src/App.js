c2d6b44a9d4dba669e7027c5b7f97d83b6902fd9
Nicholas Iloba <nick_iloba@hotmail.com>
2021-07-28 22:17:32 -0400

    finished the dropdown widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
@@ -36,11 +36,17 @@ const options = [
 
 const App = () => {
     const [selected, setSelected] = useState(options[0]);
+    const [showDropDown, setShowDropdown] = useState(true)
     return (
         <div>
+            <button onClick={() => setShowDropdown(!showDropDown)}>Toggle DropDown</button>
             {/* <Accordion items={items} /> */}
             {/* <Search /> */}
-            <Dropdown selected={selected} onSelectedChange={setSelected} options={options} />
+            {showDropDown ?
+                <Dropdown selected={selected} onSelectedChange={setSelected} options={options} />
+                : null
+            }
+
         </div>
     );
 }
