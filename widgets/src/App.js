562f57be1ff57cabc364405cf93af7d881d0950b
Nicholas Iloba <nick_iloba@hotmail.com>
2021-10-28 23:10:01 -0400

    back again

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -8,7 +8,7 @@ import Header from './components/Header';
 
 const items = [
     {
-        title: "what is React?",
+        title: "what is Reactt?",
         content: "React is a front end JS framework"
     },
     {
